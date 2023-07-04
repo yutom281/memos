0afab9dee500c6b00e487299073feb527ec577c7
snowflake <yutom281@gmail.com>
2023-07-04 23:42:13 +0900

    Minor bug fix

--- api/models.py
+++ api/models.py
@@ -15,60 +15,28 @@ class Memo(BaseModel):
         schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "date": "2022-09-30",
-                "topic": "買い物",
-                "Good/Bad": "○",
+                "date": "2023-07-04",
+                "topic": "Shopping",
+                "Good/Bad": "Good",
                 "cause": "...",
-                "lesson": "割引の商品をアプリでチェックする。"
+                "lesson": "Search for discounted items on app",
             }
         }
 
 class MemoUpdate(BaseModel):
-    date: str = Optional[str]
-    topic: str = Optional[str]
-    Good_Bad: str = Optional[str]
-    cause: str = Optional[str]
-    lesson: str = Optional[str]
+    date: Optional[str]
+    topic: Optional[str]
+    Good_Bad: Optional[str]
+    cause: Optional[str]
+    lesson: Optional[str]
 
     class Config:
         schema_extra = {
             "example": {
-                "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "date": "2022-09-30",
-                "topic": "買い物",
-                "Good/Bad": "○",
+                "date": "2023-07-04",
+                "topic": "Shopping",
+                "Good/Bad": "Good",
                 "cause": "...",
-                "lesson": "割引の商品をアプリでチェックする。"
+                "lesson": "Search for discounted items on app",
             }
         }
-
-# class Book(BaseModel):
-#     id: str = Field(default_factory=uuid.uuid4, alias="_id")
-#     title: str = Field(...)
-#     author: str = Field(...)
-#     synopsis: str = Field(...)
-
-#     class Config:
-#         allow_population_by_field_name = True
-#         schema_extra = {
-#             "example": {
-#                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-#                 "title": "Don Quixote",
-#                 "author": "Miguel de Cervantes",
-#                 "synopsis": "..."
-#             }
-#         }
-
-# class BookUpdate(BaseModel):
-#     title: Optional[str]
-#     author: Optional[str]
-#     synopsis: Optional[str]
-
-#     class Config:
-#         schema_extra = {
-#             "example": {
-#                 "title": "Don Quixote",
-#                 "author": "Miguel de Cervantes",
-#                 "synopsis": "Don Quixote is a Spanish novel by Miguel de Cervantes..."
-#             }
-#         }
--- api/routes.py
+++ api/routes.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from typing import List
-from bson import regex
 
 from models import Memo, MemoUpdate
-# from models import Book, BookUpdate
 
 router = APIRouter()
 
@@ -16,7 +14,7 @@ def list_memos(request: Request):
 
 @router.get("/{topic}", response_description="Get memos by topic, using regex", response_model=List[Memo])
 def find_memo(topic: str, request: Request):
-    memos = list(request.app.database["memos"].find({"topic": {"$regex": f".*{topic}.*"}}))
+    memos = list(request.app.database["memos"].find({"topic": {"$regex": f".*{topic}.*", "$options": "i"}}))
     return memos
 
 
@@ -27,7 +25,6 @@ def create_memo(request: Request, memo: Memo = Body(...)):
     created_memo = request.app.database["memos"].find_one(
         {"_id": new_memo.inserted_id}
     )
-
     return created_memo
 
 
@@ -59,57 +56,3 @@ def delete_memo(id: str, request: Request, response: Response):
         return response
     
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Memo with ID {id} not found")
-
-
-# @router.get("/", response_description="List all books", response_model=List[Book])
-# def list_books(request: Request):
-#     books = list(request.app.database["books"].find(limit=100))
-#     return books
-
-
-# @router.get("/{id}", response_description="Get a single book by id", response_model=List[Book])
-# def find_book(id: str, request: Request):
-#     if (book := request.app.database["books"].find_one({"_id": id})) is not None:
-#         return [book]
-#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
-
-
-# @router.post("/", response_description="Create a new book", status_code=status.HTTP_201_CREATED, response_model=Book)
-# def create_book(request: Request, book: Book = Body(...)):
-#     book = jsonable_encoder(book)
-#     new_book = request.app.database["books"].insert_one(book)
-#     created_book = request.app.database["books"].find_one(
-#         {"_id": new_book.inserted_id}
-#     )
-
-#     return created_book
-
-
-# @router.put("/{id}", response_description="Update a book", response_model=Book)
-# def update_book(id: str, request: Request, book: BookUpdate = Body(...)):
-#     book = {k: v for k, v in book.dict().items() if v is not None}
-#     if len(book) >= 1:
-#         update_result = request.app.database["books"].find_one(
-#             {"_id": id}, {"$set": book}
-#         )
-
-#         if update_result.modified_count == 0:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
-        
-#     if (
-#         existing_book := request.app.database["books"].find_one({"_id": id})
-#     ) is not None:
-#         return existing_book
-    
-#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
-
-
-# @router.delete("/{id}", response_description="Delete a book")
-# def delete_book(id: str, request: Request, response: Response):
-#     delete_result = request.app.database["books"].delete_one({"_id": id})
-
-#     if delete_result.deleted_count == 1:
-#         response.status_code = status.HTTP_204_NO_CONTENT
-#         return response
-    
-#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with ID {id} not found")
