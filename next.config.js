eab46cd92048e728a8aebfb4a0b7ef95797c88d0
yutom281 <yutom281@gmail.com>
2023-09-01 22:20:43 +0900

    Removed unimplemented route

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/memos",
-        destination: "http://localhost:3001/memos",
-      },
-      {
         source: "/memos/:query*",
         destination: "http://localhost:3001/memos/:query*",
       },
